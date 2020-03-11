ab313fca98f7cfc25b91a22398b85ef3353d5be9
ImerAl <40179626+ImerAl@users.noreply.github.com>
2020-03-10 22:18:58 -0600

    classes models

---
+++ sistemaDeGestionDeProyectosEIncidencias/src/sv/edu/udb/models/Projects.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.udb.models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Imer Palma
+ */
+public class Projects {
+    private int id, departmentId;
+    private String name, description;
+    private Date creationDate;
+    
+    public Projects(){}
+    
+    public void setProjectsId(int projectsId){
+        this.id = projectsId;
+    }
+    public int getProjectsId(){
+        return id;
+    }
+    public void setDepartmentId(int departmentId){
+        this.departmentId = departmentId;
+    }
+    public int getDepartmentId(){
+        return departmentId;
+    }
+    public void setProjectName(String name){
+        this.name = name;
+    }
+    public String getProjectName(){
+        return name;
+    }
+    public void setProjectDescription(String description){
+        this.description = description;
+    }
+    public String getProjectDescription(){
+        return description;
+    }
+    public void setCreationDate(Date date){
+        this.creationDate = date;
+    }
+    public Date getCreationDate(){
+        return creationDate;
+    }
+    
+}
--- sistemaDeGestionDeProyectosEIncidencias/src/sv/edu/udb/models/Ticket.java
+++ sistemaDeGestionDeProyectosEIncidencias/src/sv/edu/udb/models/Ticket.java
@@ -4,14 +4,66 @@
  * and open the template in the editor.
  */
 package sv.edu.udb.models;
-
+import java.util.*;
 /**
  *
  * @author Imer
  */
 public class Ticket {
-    
+    private int id, request, programmer, tester;
+    private String ticketStatus, internalCode;
+    private Date startDate, endDate;
     public Ticket(){
     }
+    public void setIdTicket(int ticketId){
+        this.id = ticketId;
+    }
+    public int getIdTicket(){
+        return id;
+    }
+    public void setRequestId(int requestTicket){
+        this.request = requestTicket;
+    }
+    public int getRequestId(){
+        return request;
+    }
+    public void setIdProgrammer(int programmer){
+        this.programmer = programmer;
+    }
+    public int getIdProgrammer(){
+        return programmer;
+    }
+    public void setIdTester(int idTester){
+        this.tester = idTester;
+    }
+    public int getIdTester(){
+        return tester;
+    }
+    public void setTicketStatus(String status){
+        this.ticketStatus = status;
+    }
+    public String getTicketStatus(){
+        return ticketStatus;
+    }
+    public void setInternalCode(String internalCode){
+        this.internalCode = internalCode;
+    }
+    public String getInternalCode(){
+        return internalCode;
+    }
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+    public Date getStartDate(){
+        return startDate;
+    }
+    public void setEndDate(Date endDate){
+        this.endDate = endDate;
+    }
+    public Date getEndDate(){
+        return endDate;
+    }
+    
+    
     
 }
